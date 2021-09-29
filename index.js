629bcfa0fdcf3ee6a2a667f55c48fad74fd3fb62
James Capadocia <james.capadocia@gmail.com>
2021-09-29 15:40:24 -0500

    Created and wrote code for all .js files to communicate(import/export) to each other as needed. At least I think I did it properly.... will need to start writing code for class constructors and unit tests with placeholders to confirm.

---
+++ index.js
@@ -0,0 +1,6 @@
+const generateHTML = require('./src/generateHTML');
+const fs = require('fs'); 
+const inquirer = require('inquirer');
+const Manager = require('./lib/Manager.constructor');
+const Engineer = require('./lib/Engineer.constructor');
+const Intern = require('./lib/Intern.constructor'); 
