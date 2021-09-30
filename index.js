0e285a2142f28a288fb695b7ab7b0aab72661766
James Capadocia <james.capadocia@gmail.com>
2021-09-29 22:34:19 -0500

    Created empty array and prompts for user input. Still need to write code for 'confirm' input to restart prompts if 'true' and for user input to create and push an employee (based on user input) into the empty array.

--- index.js
+++ index.js
@@ -1,6 +1,61 @@
-const generateHTML = require('./src/generateHTML');
-const fs = require('fs'); 
-const inquirer = require('inquirer');
-const Manager = require('./lib/Manager.constructor');
-const Engineer = require('./lib/Engineer.constructor');
-const Intern = require('./lib/Intern.constructor'); 
+// const generateHTML = require("./src/generateHTML");
+// const fs = require("fs");
+const inquirer = require("inquirer");
+// const Manager = require("./lib/Manager.constructor");
+// const Engineer = require("./lib/Engineer.constructor");
+// const Intern = require("./lib/Intern.constructor");
+
+const teamArray = [];
+
+const addEmployee = () => {
+  return inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "role",
+        message: "What is the employee's role?",
+        choices: ["Manager", "Engineer", "Intern"],
+      },
+      {
+        type: "input",
+        name: "name",
+        message: "What is the employee's name?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What is the employee's ID?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is the employee's email?",
+      },
+      {
+        type: "input",
+        name: "officeNum",
+        message: "What is the manager's office number?",
+        when: (input) => input.role === "Manager",
+      },
+      {
+        type: "input",
+        name: "gitHub",
+        message: "What is the engineer's GitHub username?",
+        when: (input) => input.role === "Engineer",
+      },
+      {
+        type: "input",
+        name: "school",
+        message: "What school does the intern attend?",
+        when: (input) => input.role === "Intern",
+      },
+      {
+        type: "confirm",
+        name: "confirmAdd",
+        message: "Do you need to add more team members?",
+        default: false,
+      },
+    ])
+};
+
+addEmployee()
