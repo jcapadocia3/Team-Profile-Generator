8aa45210eee26ee2b01836c7d4eb707abaab8000
James Capadocia <james.capadocia@gmail.com>
2021-09-30 15:04:37 -0500

    Created tests for both 'manager' and 'employee' objects and data retrieved from user input -- both passed. Still need to create tests for 'engineer' and 'intern' as well as update the 'employee' test with 'github' and 'school' properties to test as well.

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,2 +1,20 @@
 const Employee = require('../lib/Employee.constructor');
 
+test('creates an employee object', () => {
+    const employee = new Employee('James', 11, 'jcapado@gmail.com');
+
+    expect(employee.name).toEqual(expect.any(String));
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.any(String));
+});
+
+test('retrieve ALL employee data', () => {
+    const employee = new Employee('James', 11, 'jcapado@gmail.com');
+
+    expect(employee.dataName()).toEqual(expect.any(String));
+    expect(employee.dataId()).toEqual(expect.any(Number));
+    expect(employee.dataEmail()).toEqual(expect.stringContaining(employee.email.toString()));
+    expect(employee.dataRole()).toEqual("Employee");
+});
+
+// still need to write test for Engineer "gitHub" property and Intern "school" property
--- _tests_/Manager.test.js
+++ _tests_/Manager.test.js
@@ -1,2 +1,11 @@
 const Manager = require('../lib/Manager.constructor');
 
+test('creates Manager object', () => {
+    const manager = new Manager('James', 11, 'jcapado@gmail.com', 1);
+    
+    expect(manager.name).toEqual(expect.any(String));
+    expect(manager.id).toEqual(expect.any(Number));
+    expect(manager.email).toEqual(expect.any(String));
+    expect(manager.officeNum).toEqual(expect.any(Number));
+    expect(manager.dataRole()).toEqual("Manager");
+});
