9026ee3da8fc3141fa86b2b8770bbf5fadb87033
James Capadocia <james.capadocia@gmail.com>
2021-10-01 18:48:37 -0500

    Cleaned up code in constructor files. Commented all constructor files to help user understand how code functions.

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -6,13 +6,5 @@ test('creates an employee object', () => {
     expect(employee.name).toEqual(expect.any(String));
     expect(employee.id).toEqual(expect.any(Number));
     expect(employee.email).toEqual(expect.any(String));
-});
-
-test('tests ALL alike employee data', () => {
-    const employee = new Employee('James', 11, 'jcapado@gmail.com');
-
-    expect(employee.dataName()).toEqual(expect.any(String));
-    expect(employee.dataId()).toEqual(expect.any(Number));
-    expect(employee.dataEmail()).toEqual(expect.stringContaining(employee.email.toString()));
     expect(employee.dataRole()).toEqual("Employee");
 });
